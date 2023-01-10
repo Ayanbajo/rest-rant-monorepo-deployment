2368123026a26c6416b87f91a2af72983251f318
ayanbajo <bolatitoayanbajo@gmail.com>
2023-01-09 19:09:09 -0500

    restrant aws deployment

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const app = express();
+
+// Express Settings
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'public', 'build')));
+}
+
+// Controllers & Routes
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/places', require('./controllers/places'))
+app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
+
+// Listen for Connections
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`)
+})
